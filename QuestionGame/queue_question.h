eb69f31fd8de5841e07a03d8f0b8fe111539e24a
AlexandrPirogov <cat_shot12@mail.ru>
2022-10-05 00:35:14 +0300

    init

---
+++ QuestionGame/queue_question.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <map>
+#include <queue>
+
+class Question
+{
+	short correctAnswer;
+	std::string question;
+	std::map<int, std::string> possibleAnswers;
+public:
+	Question() = delete;
+	Question(std::string question, std::map<int, std::string> possibleAnswers, short correctAnswer)
+		: possibleAnswers(possibleAnswers), question(question), correctAnswer(correctAnswer) {};
+
+	inline const std::string whatQuestion()
+	{
+		return question;
+	}
+
+	inline const std::map<int, std::string> whatPossibleAnswers()
+	{
+		return possibleAnswers;
+	}
+
+	inline const short whatCorrectAnswer()
+	{
+		return correctAnswer;
+	}
+};
+
+class QuestionQueue
+{
+	short add_status;
+	short answer_status;
+	std::queue<Question> questions;
+public:
+
+	short const ADD_STATUS_NIL = -1;
+	short const ADD_STATUS_OK = 0;
+
+	short const ANSWER_STATUS_NIL = -1;
+	short const ANSWER_STATUS_OK = 0;
+	short const ANSWER_STATUS_INCORRECT = 1;
+	short const ANSWER_STATUS_ERR = 2;
+
+	QuestionQueue()
+	{
+		add_status = ADD_STATUS_NIL;
+		answer_status = ANSWER_STATUS_NIL;
+	};
+
+	inline const size_t size() noexcept
+	{
+		return questions.size();
+	}
+
+	inline const short addStatus() noexcept
+	{
+		return add_status;
+	}
+
+	inline const short answerStatus() noexcept
+	{
+		return answer_status;
+	}
+
+};
+
---
+++ QuestionsTest/test.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "./../QuestionGame/queue_question.h"
+#include <memory>
+#include <string>
+#include <map>
+
+namespace QueueTests
+{
+	std::shared_ptr<Question> createQuestion()
+	{
+		std::string quesStr = "Question?";
+		std::map<int, std::string> answers = { {1, "1"}, {2, "2"}, {3, "3"} };
+		int correct = 1;
+		std::shared_ptr<Question> ques = std::make_shared<Question>(quesStr, answers, correct);
+		assert(ques->whatCorrectAnswer() == correct);
+		assert(ques->whatQuestion()  == quesStr);
+		assert(ques->whatPossibleAnswers() == answers);
+		return ques;
+	}
+
+	TEST(TestCaseName, TestName) {
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+	}
+
+	TEST(QuestionTests, CreatingTest) {
+		std::shared_ptr<Question> question = createQuestion();
+	}
+
+	TEST(QueueTests, CreatingAndFillingQueue)
+	{
+		std::unique_ptr<QuestionQueue> queue = std::make_unique<QuestionQueue>();
+		ASSERT_TRUE(queue->size() == 0);
+		ASSERT_TRUE(queue->addStatus() == queue->ADD_STATUS_NIL);
+		ASSERT_TRUE(queue->answerStatus() == queue->ANSWER_STATUS_NIL);
+	}
+
+}
+
+
+
